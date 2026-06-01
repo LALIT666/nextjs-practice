cd868b2cd2c4721991e0ea320bc496c013efadb6
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-06-01 17:50:34 +0200

    1.6 --- 2

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -2,21 +2,40 @@
 
 import React, { useState } from "react";
 
+interface InputObj {
+  name: string;
+  email: string;
+}
+
 export default function FormMuscelMemory() {
-  //2nd time
-  const [name, setName] = useState("");
+  //"2nd time" multiple inputs
+
+  const [form, setForm] = useState({ name: "", email: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e: React.SubmitEvent) => {
     e.preventDefault();
-    console.log("Submited String: ", name);
+    console.log(form);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+        placeholder="joe doe"
+      />
+      <input
+        type="text"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+        placeholder="joedoe@gmail.com"
       />
       <button type="submit">Submit</button>
     </form>
