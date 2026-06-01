aca1335d57d6f358dfc0bf27dcd78340b59a00ba
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-06-01 20:34:31 +0200

    1.6 --- 11

--- src/app/hooks/useeffecthook/page.tsx
+++ src/app/hooks/useeffecthook/page.tsx
@@ -2,14 +2,14 @@
 
 import { useEffect, useState } from "react";
 
-//runs on every render
+//runs when counts change
 
 export default function HomeHooks() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     alert(`Rendere, count: ${count} `);
-  });
+  }, [count]);
 
   return <button onClick={() => setCount(count + 1)}>{count}</button>;
 }
