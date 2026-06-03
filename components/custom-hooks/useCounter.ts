d924361101a5c07ca9ea9762cb18abd3d40a015b
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-06-03 13:44:20 +0200

    3.6 --- 8

---
+++ components/custom-hooks/useCounter.ts
@@ -0,0 +1,17 @@
+import { useCallback, useState } from "react";
+
+export function useCounter(initialCount = 0) {
+  const [count, setCount] = useState(initialCount);
+
+  const inc = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, []);
+  const dec = useCallback(() => {
+    setCount((prevCount) => prevCount - 1);
+  }, []);
+  const reset = useCallback(() => {
+    setCount((prevCount) => initialCount);
+  }, [initialCount]);
+
+  return { count, inc, dec, reset };
+}
--- src/app/hooks/customHooks/page.tsx
+++ src/app/hooks/customHooks/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { useCounter } from "../../../../components/custom-hooks/useCounter";
 import { useToggle } from "../../../../components/custom-hooks/useToggle";
 
 export default function HomeCustomHooks() {
   const { value, toggle } = useToggle();
+  const { count, inc, dec, reset } = useCounter(5);
   return (
     <div className="flex flex-col h-screen items-center justify-center gap-3">
       <h1>Custom Hooks</h1>
 
       <button onClick={toggle}>{value ? "ON" : "OFF"}</button>
+
+      <h1>UseCounter</h1>
+      <p>Count: {count}</p>
+      <button onClick={inc}>Inc</button>
+      <button onClick={dec}>Dec</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
