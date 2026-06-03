709677648165e62847a453561269d2ce6b53a10d
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-06-03 10:34:19 +0200

    3.6 --- 6

--- src/app/hooks/useeffecthook/page.tsx
+++ src/app/hooks/useeffecthook/page.tsx
@@ -1,63 +1,42 @@
-//🟢 DRILL 12: Final Boss — Axios + useEffect + Loading + Dependency
-//
-
 "use client";
 
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-type Post = {
-  id: number;
-  title: string;
-};
-
 export default function HomeHooks() {
-  const [userId, setUserId] = useState(1);
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-
+  const [loading, setLoading] = useState(false);
+  const [quote, setQuote] = useState("");
   useEffect(() => {
-    const loadUserPost = async () => {
+    const fetchingQuote = async () => {
       try {
         setLoading(true);
         setError("");
-        const res = await axios.get(
-          `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
-        );
-        console.log(`res.data: ${res.data}`);
 
-        setPosts(res.data);
-
-        console.log(
-          `{this is in useeffect after setpost(res.data):  ${posts}}`,
-        );
+        const res = await axios.get("https://api.quotable.io/random");
+        setQuote(res.data.content);
       } catch {
-        setError("Erron in loading user's post");
+        setError("Error....");
       } finally {
         setLoading(false);
       }
     };
+    fetchingQuote();
 
-    loadUserPost();
-  }, [userId]);
+    const id = setInterval(() => {
+      fetchingQuote();
+    }, 5000);
 
-  console.log({ posts });
+    return () => clearInterval(id);
+  }, []);
+
+  console.log("quote", quote);
+
   return (
     <div>
-      <input
-        type="number"
-        value={userId}
-        onChange={(e) => setUserId(Number(e.target.value))}
-      />
-      {loading && <p>Loading...</p>}
+      {loading && <p>Loading....</p>}
       {error && <p>{error}</p>}
-
-      {posts.map((post, index) => (
-        <p key={post.id}>
-          title no: {index + 1} -- {post.title}
-        </p>
-      ))}
+      <p>Quote: {quote}</p>
     </div>
   );
 }
