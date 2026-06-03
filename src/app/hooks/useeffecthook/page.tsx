d2b769fb280cbd9a1db6d71fb011e601e71ef797
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-06-03 09:55:11 +0200

    3.6 --- 5

--- src/app/hooks/useeffecthook/page.tsx
+++ src/app/hooks/useeffecthook/page.tsx
@@ -1,29 +1,63 @@
-// 🟢 DRILL 11: Interval Start/Stop
+//🟢 DRILL 12: Final Boss — Axios + useEffect + Loading + Dependency
+//
 
 "use client";
 
+import axios from "axios";
 import { useEffect, useState } from "react";
 
-export default function HomeHook() {
-  const [time, setTime] = useState(0);
-  const [running, setRunning] = useState(true);
+type Post = {
+  id: number;
+  title: string;
+};
+
+export default function HomeHooks() {
+  const [userId, setUserId] = useState(1);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    if (!running) return;
+    const loadUserPost = async () => {
+      try {
+        setLoading(true);
+        setError("");
+        const res = await axios.get(
+          `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+        );
+        console.log(`res.data: ${res.data}`);
 
-    const id = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
-    }, 100);
+        setPosts(res.data);
 
-    return () => clearInterval(id);
-  }, [running]);
+        console.log(
+          `{this is in useeffect after setpost(res.data):  ${posts}}`,
+        );
+      } catch {
+        setError("Erron in loading user's post");
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    loadUserPost();
+  }, [userId]);
+
+  console.log({ posts });
   return (
     <div>
-      <p>Time: {time}</p>
-      <button onClick={() => setRunning(!running)}>
-        {running ? "STOP" : "START"}
-      </button>
+      <input
+        type="number"
+        value={userId}
+        onChange={(e) => setUserId(Number(e.target.value))}
+      />
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      {posts.map((post, index) => (
+        <p key={post.id}>
+          title no: {index + 1} -- {post.title}
+        </p>
+      ))}
     </div>
   );
 }
